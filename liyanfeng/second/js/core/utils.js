e9b71977e61802b40f62366f995fc2c41ae3b319
muzi131313 <muzi131313@163.com>
2016-03-24 10:04:28 +0800

    something funny

---
+++ liyanfeng/second/js/core/utils.js
@@ -0,0 +1,8 @@
+/**
+  * 2016 JavaScript technology stack:
+  * http://www.codeceo.com/article/2016-javascript-stack-state.html
+  */
+/**
+  * article list:
+  * http://www.codeceo.com/article/tag/javascript
+  */
